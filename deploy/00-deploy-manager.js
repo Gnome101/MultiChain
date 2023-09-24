312fb402108b176febfc4d119c9bba246057e469
Gnome101 <thegnome@FurryBeast>
2023-09-24 07:09:58 -0700

    another

--- deploy/00-deploy-manager.js
+++ deploy/00-deploy-manager.js
@@ -25,38 +25,38 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
       "@uniswap/v4-core/contracts/PoolManager.sol:PoolManager"
     );
   }
-  // args = ["0x693B1C9fBb10bA64F0d97AE042Ee32aE9Eb5610D"];
-  // const Router = await deploy("UniswapInteract", {
-  //   from: deployer,
-  //   args: args,
-  //   log: true,
-  //   blockConfirmations: 2,
-  // });
-  // console.log("Chain", chainId);
-  // if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
-  //   log("Verifying...");
-  //   await verify(
-  //     Router.address,
-  //     args,
-  //     "contracts/UniswapInteract.sol:UniswapInteract"
-  //   );
-  // }
-  // args = [];
-  // const HookFactory = await deploy("UniswapHooksFactory", {
-  //   from: deployer,
-  //   args: args,
-  //   log: true,
-  //   blockConfirmations: 2,
-  // });
+  args = ["0x693B1C9fBb10bA64F0d97AE042Ee32aE9Eb5610D"];
+  const Router = await deploy("UniswapInteract", {
+    from: deployer,
+    args: args,
+    log: true,
+    blockConfirmations: 2,
+  });
+  console.log("Chain", chainId);
+  if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(
+      Router.address,
+      args,
+      "contracts/UniswapInteract.sol:UniswapInteract"
+    );
+  }
+  args = [];
+  const HookFactory = await deploy("UniswapHooksFactory", {
+    from: deployer,
+    args: args,
+    log: true,
+    blockConfirmations: 2,
+  });
 
-  // console.log("Chain", chainId);
-  // if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
-  //   log("Verifying...");
-  //   await verify(
-  //     HookFactory.address,
-  //     args,
-  //     "contracts/Utils/HooksFactory.sol:UniswapHooksFactory"
-  //   );
-  // }
+  console.log("Chain", chainId);
+  if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(
+      HookFactory.address,
+      args,
+      "contracts/Utils/HooksFactory.sol:UniswapHooksFactory"
+    );
+  }
 };
 module.exports.tags = ["all", "Pool"];
